8876a7f4849cedfff14e3a35e5fc494e616836f2
j0verton <overtonje@gmail.com>
2020-08-19 10:58:18 -0500

    t
    added fucntions from assignment

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -546,26 +546,18 @@ const fishCollection = [
         diet: "small fish",
         harvestLocation: "3rd Sea",
     }
-
-
-
-
-
-
-
-
-
 ]
 
+export const useFish = () => {
+    return fishCollection.slice()
 
-for (fish of fishCollection) {
-    let fishCard = `<div><img src=${fish.image}></div><ul>`;
-    fishCard += `<li>Given Name: ${fish.name}</li>`;
-    fishCard += `<li>Species: ${fish.species}</li>`;
-    fishCard += `<li>length: ${fish.length}</li>`;
-    fishCard += `<li>Diet: ${fish.food}</li>`;
-    fishCard += `<li>Harvest Location: ${fish.harvestLocation}</li>`;
-    fishCard += `</ul>`;
-    return fish,
-}
-
+// for (fish of fishCollection) {
+//     let fishCard = `<div><img src=${fish.image}></div><ul>`;
+//     fishCard += `<li>Given Name: ${fish.name}</li>`;
+//     fishCard += `<li>Species: ${fish.species}</li>`;
+//     fishCard += `<li>length: ${fish.length}</li>`;
+//     fishCard += `<li>Diet: ${fish.food}</li>`;
+//     fishCard += `<li>Harvest Location: ${fish.harvestLocation}</li>`;
+//     fishCard += `</ul>`;
+//     return fish,
+// };
